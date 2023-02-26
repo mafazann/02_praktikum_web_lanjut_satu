56af4b68e904b7d91e504dc73b3dbebb2df427b6
mafazann <fafanofficialyt@gmail.com>
2023-02-26 18:35:38 +0700

    first commit

---
+++ 02_praktikum_web_lanjut_satu/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "2141720140 <br>";
+    echo "M Rizky Mafazan <br>";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID ".($id) ;
+});
